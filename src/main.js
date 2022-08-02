0fe43338dc27867fa8dd031ff99cc989a3434163
quan <304390323@qq.com>
2022-08-02 16:10:45 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,12 @@
+const mouthDiv = document.querySelectorAll(".down")[0];
+const pikaPng = document.querySelector("#pikaPng");
+console.log(pikaPng);
+mouthDiv.onclick = (e) => {
+  const audio = document.querySelector("#pikaAudio");
+  audio.autoplay = true;
+  audio.play();
+  pikaPng.style = "display:block;";
+  setTimeout(() => {
+    pikaPng.style = "display:none;";
+  }, 1500);
+};
